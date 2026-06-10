e38c0c289395639247362a7eb03a13743c98cdf8
liert <2102156704@qq.com>
2026-06-10 20:45:21 +0800

    fix: escape string constants in AstPrinter to prevent physical newlines in Lua output

--- src/main/java/com/github/relua/ast/AstPrinter.java
+++ src/main/java/com/github/relua/ast/AstPrinter.java
@@ -243,7 +243,42 @@ public class AstPrinter implements AstVisitor<String> {
     
     @Override
     public String visit(StringConst node) {
-        return "\"" + node.value + "\"";
+        return "\"" + escapeLuaString(node.value) + "\"";
+    }
+
+    private String escapeLuaString(String value) {
+        if (value == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < value.length(); i++) {
+            char c = value.charAt(i);
+            switch (c) {
+                case '\\':
+                    sb.append("\\\\");
+                    break;
+                case '"':
+                    sb.append("\\\"");
+                    break;
+                case '\n':
+                    sb.append("\\n");
+                    break;
+                case '\r':
+                    sb.append("\\r");
+                    break;
+                case '\t':
+                    sb.append("\\t");
+                    break;
+                default:
+                    if (c < 32) {
+                        sb.append(String.format("\\%03d", (int) c));
+                    } else {
+                        sb.append(c);
+                    }
+                    break;
+            }
+        }
+        return sb.toString();
     }
     
     @Override
